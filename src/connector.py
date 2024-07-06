e77362fe158a2c2fc4880d1c8ee6c773d77d0e4d
DStefanow <dobromirstefanov96@abv.bg>
2024-07-07 00:40:24 +0300

    Change the logic for environment variables to allow loading them from the system

--- src/connector.py
+++ src/connector.py
@@ -4,16 +4,20 @@ import os, time
 from dotenv import load_dotenv
 import psycopg2
 
-# Load environment variable from .env file in root folder
-load_dotenv("../.env")
+def load_env_vars(dotenv_path: str) -> list:
+    # Load environment variable from .env file in root folder if exists
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
 
-PG_HOST = os.getenv('PG_HOST')
-PG_DATABASE = os.getenv('PG_DATABASE')
-PG_USER = os.getenv('PG_USER')
-PG_PASSWORD = os.getenv('PG_PASSWORD')
-PG_PORT = os.getenv('PG_PORT')
+    PG_HOST = os.getenv('PG_HOST')
+    PG_DATABASE = os.getenv('PG_DATABASE')
+    PG_USER = os.getenv('PG_USER')
+    PG_PASSWORD = os.getenv('PG_PASSWORD')
+    PG_PORT = os.getenv('PG_PORT')
 
-def pg_connection(pg_host:str, pg_db: str, pg_user: str, pg_pass: str, pg_port: int):
+    return (PG_HOST, PG_DATABASE, PG_USER, PG_PASSWORD, PG_PORT)
+
+def pg_connection(pg_host:str, pg_db: str, pg_user: str, pg_pass: str, pg_port: int) -> object:
     # Try to connect to the databse
     try:
         connector = psycopg2.connect(
@@ -29,6 +33,11 @@ def pg_connection(pg_host:str, pg_db: str, pg_user: str, pg_pass: str, pg_port: 
     return connector
    
 if __name__ == "__main__":
+    dotenv_location = "../.env"
+
+    # Load environment variables and exit if they are missing
+    ( PG_HOST, PG_DATABASE, PG_USER, PG_PASSWORD, PG_PORT ) = load_env_vars(dotenv_location)
+
     # Create connection to the database
     pg_connector = pg_connection(pg_host=PG_HOST, pg_db=PG_DATABASE, pg_user=PG_USER, pg_pass=PG_PASSWORD, pg_port=PG_PORT)
 
